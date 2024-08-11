748776e5401dc7635954db7ef1fd4c6adf54b592
GreatestCoder <namansharma9858581009@gmail.com>
2024-08-11 17:47:42 +0530

    Made a few changes.

--- Portfolio-Project/Portfolio.py
+++ Portfolio-Project/Portfolio.py
@@ -3,10 +3,16 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image(r"C:\Users\LENOVO\Documents\Python\Projects\Portfolio-Project\2.png")
+    st.image(r"C:\Users\LENOVO\Documents\Python\Projects\Portfolio-Project\Self.png")
 with col2:
     st.title("My Info")
     content = """
     Blah Blah Blah
     """
     st.info(content)
+
+content2 = """
+Below you can find some Python Projects built by me.
+"""
+st.info(content2)
+
