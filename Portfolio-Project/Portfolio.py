df4c6624254b9bddb9cc3acb88185f9cbedc1ebb
GreatestCoder <namansharma9858581009@gmail.com>
2024-08-11 18:31:29 +0530

    Layout Finished

--- Portfolio-Project/Portfolio.py
+++ Portfolio-Project/Portfolio.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.set_page_config(layout="wide")
+import pandas as pd
 
 col1, col2 = st.columns(2)
 with col1:
@@ -7,7 +8,7 @@ with col1:
 with col2:
     st.title("My Info")
     content = """
-    Blah Blah Blah
+    Hi! My name is Naman Sharma and I am a college student studying at Amity University,Noida in India.
     """
     st.info(content)
 
@@ -16,3 +17,18 @@ Below you can find some Python Projects built by me.
 """
 st.info(content2)
 
+df = pd.read_csv(r"C:\Users\LENOVO\Documents\Python\Projects\Portfolio-Project\data.csv", sep=";")
+
+col3, empty_col, col4 = st.columns([1.5,0.5,1.5])
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(r"C:\Users\LENOVO\Documents\Python\Projects\Portfolio-Project\images/" + row["image"])
+        st.write(f"[Source Code]({row["url"]})")
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(r"C:\Users\LENOVO\Documents\Python\Projects\Portfolio-Project\images/" + row["image"])
+        st.write(f"[Source Code]({row["url"]})")
